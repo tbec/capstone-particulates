c137b999207675134109c2f82e3fc738e653ab33
Trevor Chapman <tchapman121@gmail.com>
2018-11-17 16:32:50 -0700

    Prettied home screen

--- AirU_Mobile/Views/Home.js
+++ AirU_Mobile/Views/Home.js
@@ -1,53 +1,54 @@
 import React, {Component} from 'react';
-import {View, TouchableHighlight, Text, Image, Touchable} from 'react-native';
+import {View, TouchableHighlight, Text, ImageBackground, Image} from 'react-native';
 import {NavigationActions} from 'react-navigation'
 import styles from '../StyleSheets/Styles'
 
 export default class Home extends Component<Props> {
     // need way to check if already registered and change options accordingly
-    // need to have display background image
-    
+
     render() {
         return (
                 <View style={styles.container}>
-                    <View style={styles.header}>
-                    {/* header image goes here */}
-                    </View>
-                    <View style={styles.home}>
-                    <TouchableHighlight 
-                        style={styles.button}
-                        onPress={() => this.props.navigation.navigate({
-                            routeName: 'Tabs',
-                            params: {},
-                            action: NavigationActions.navigate({routeName: 'Setup'})})}>
-                            <Text>Setup Sensor</Text>
-                    </TouchableHighlight>
-                    <TouchableHighlight 
-                        style={styles.button}
-                        onPress={() => this.props.navigation.navigate({
-                            routeName: 'Tabs',
-                            params: {},
-                            action: NavigationActions.navigate({routeName: 'Tracker'})})}>
-                            <Text>Personal Tracker</Text>
-                    </TouchableHighlight>
-                    <TouchableHighlight 
-                        style={styles.button}
-                        onPress={() => this.props.navigation.navigate({
-                            routeName: 'Tabs',
-                            params: {},
-                            action: NavigationActions.navigate({routeName: 'Map'})})}>
-                            <Text>Sensor Map</Text>
-                    </TouchableHighlight>
-                    <TouchableHighlight 
-                        style={styles.button}
-                        onPress={() => this.props.navigation.navigate({
-                            routeName: 'Tabs',
-                            params: {},
-                            action: NavigationActions.navigate({routeName: 'Settings'})})}>
-                            <Text>Settings</Text>
-                    </TouchableHighlight>
+                    <ImageBackground source={require('../Resources/home_background.jpg')} style={{width: '100%', height: '100%'}}>
+                        <View style={styles.header}>
+                            <Image source={require('../Resources/AQ_Logo.png')} style={{width: '100%', height: '100%'}}/>
+                        </View>
+                        <View style={styles.home}>
+                            <TouchableHighlight 
+                                style={styles.button}
+                                onPress={() => this.props.navigation.navigate({
+                                    routeName: 'Tabs',
+                                    params: {},
+                                    action: NavigationActions.navigate({routeName: 'Setup'})})}>
+                                    <Text>Setup Sensor</Text>
+                            </TouchableHighlight>
+                            <TouchableHighlight 
+                                style={styles.button}
+                                onPress={() => this.props.navigation.navigate({
+                                    routeName: 'Tabs',
+                                    params: {},
+                                    action: NavigationActions.navigate({routeName: 'Tracker'})})}>
+                                    <Text>Personal Tracker</Text>
+                            </TouchableHighlight>
+                            <TouchableHighlight 
+                                style={styles.button}
+                                onPress={() => this.props.navigation.navigate({
+                                    routeName: 'Tabs',
+                                    params: {},
+                                    action: NavigationActions.navigate({routeName: 'Map'})})}>
+                                    <Text>Sensor Map</Text>
+                            </TouchableHighlight>
+                            <TouchableHighlight 
+                                style={styles.button}
+                                onPress={() => this.props.navigation.navigate({
+                                    routeName: 'Tabs',
+                                    params: {},
+                                    action: NavigationActions.navigate({routeName: 'Settings'})})}>
+                                    <Text>Settings</Text>
+                            </TouchableHighlight>
+                            </View>
+                        </ImageBackground>
                 </View>
-            </View>
         );
     }
 }
