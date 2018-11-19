b250d956c72fd9b81f7a693f178f45879367eb6d
Trevor Chapman <tchapman121@gmail.com>
2018-11-19 12:09:21 -0700

    Logic for SetupNew and Login

--- AirU_Mobile/Views/Setup/Review.js
+++ AirU_Mobile/Views/Setup/Review.js
@@ -1,1 +1,17 @@
-// shows contents of box, review then click next
+/**
+ * Main setup page
+ */
+
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+
+export default class Review extends Component<Props> {
+
+    render() {
+        return (
+            <View style={{flex: 1}}>
+                <Text>Review web page here</Text>
+            </View>
+        );
+    }
+}
--- AirU_Mobile/Views/Setup/SetupNew.js
+++ AirU_Mobile/Views/Setup/SetupNew.js
@@ -1,16 +1,47 @@
 /**
- * Main setup page
+ * Main setup page, navigates to login if not logged in otherwise navigates to first part of process (Review)
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {View, Text} from 'react-native';
+import Login from './Login'
+import Review from './Review'
 
 export default class SetupNew extends Component<Props> {
+
+    // checks if user has logged in previously
+    constructor(Props) {
+        super(Props);
+        _retrieveData = async () => {
+            try {
+              const value = await AsyncStorage.getItem('LOGIN');
+              if (value !== null) {
+                // We have data!!
+                console.log(value);
+              }
+             } catch (error) {
+               // Error retrieving data
+             }
+          }
+
+          if (_retrieveData == null) {
+            this.state={ loggedIn: false}
+          }
+          else {
+              this.state={loggedIn: true}
+          }
+    }
+
+    // If logged in already go to review, otherwise have login first
     render() {
-        return (
-            <View>
-                <Text>This is the Setup screen</Text>
-            </View>
-        );
+        if (this.state.loggedIn) {
+            return (
+                <Review/>
+            );
+        } else {
+            return (
+              <Login/>  
+            )
+        }
     }
 }
