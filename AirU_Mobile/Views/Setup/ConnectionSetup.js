f3c35cb6284e335e484cd9ad94e0261218ad31ca
Trevor Chapman <tchapman121@gmail.com>
2018-12-03 10:08:29 -0700

    Connection working on Android

--- AirU_Mobile/Views/Setup/ConnectionSetup.js
+++ AirU_Mobile/Views/Setup/ConnectionSetup.js
@@ -7,7 +7,8 @@ export default class ConnectionSetup extends Component<Props> {
     constructor(props) {
         super(props)
         // timer for faking
-        this.updateTimer = this.updateTimer.bind(this)
+        this.updateTimer = this.updateTimer.bind(this);
+        this.connectToWiFi = this.connectToWiFi.bind(this);
         let _timer = setInterval(this.updateTimer, 5000);
 
         this.state={bleConnected: false, MAC: null, wifiConnected: false, 
@@ -56,46 +57,51 @@ export default class ConnectionSetup extends Component<Props> {
             macAddress=<Image source={require('../../Resources/Loading.gif')} 
                                 style={{width: 100, height: 100, alignContent: 'center', justifyContent: 'center'}}/>
         }
-
+        
         return (
             <View style={styles.mainView}>
-                <View style={{flex: 3}}>
+                <View style={{flex: 2}}>
                     <Text>Enable your Bluetooth and connect to the sensor. Once it is connected the device name will show below. 
                         Select your WiFi network to connect to, enter the password, then select 'Connect'.
                     </Text>
                 </View>
                 {/* BLE name goes here */}
-                <View style={{flex: 3}}>
-                    <Text>{macAddress}</Text>
+                <View style={{flex: 2, jusifyContent: 'flex-start', alignContent: 'flex-start'}}>
+                    {macAddress}
                 </View>
-                <View style={{flex: 3}}>
+                <View style={{flex: 2}}>
                     <Text>Confirm the MAC ID listed above matches the one on your sensor. If it is correct, 
                         enter WiFi information below to connect the sensor to your network.
                     </Text>
                 </View>
-                <View style={{flex: 5, alignContent: 'space-around', justifyContent: 'space-between', 
+                <View style={{flex: 3, alignContent: 'flex-start', justifyContent: 'flex-start',
                         paddingLeft: 30}}>
                     {/* network */}
                     <TextInput editable={true} keyboardType='default' 
                                 autoCorrect={false} placeholder='SSID' secureTextEntry={false}
                                 style={{borderWidth: 2, borderColor: 'black', 
-                                width: '50%', alignContent: 'center', justifyContent: 'center'}}
+                                width: '50%', height: 40, alignContent: 'center', justifyContent: 'center', paddingBottom: 10}}
                                 onChangeText={(value) => {this.setState({WiFiName: value})}}
                                 />
+                    <Text/>
                     {/* password */}
                     <TextInput editable={true} keyboardType='default' 
                                 autoCorrect={false} placeholder='Password' secureTextEntry={true} 
                                 style={{borderWidth: 2, borderColor: 'black', 
-                                width: '50%', alignContent: 'center', justifyContent: 'center'}}
+                                width: '50%', height: 40, alignContent: 'center', justifyContent: 'center', paddingBottom: 10}}
                                 onChangeText={(value) => {this.setState({WiFiPassword: value})}}
                                 />
+                </View>
+                <View>
                     {/* connect button */}
-                    <Button 
-                        style={styles.button}
-                        onPress={() => this.connectToWiFi()} 
-                        title="Connect"
-                        >
-                    </Button>
+                    <Button title="Connect"
+                        onPress={() => this.connectToWiFi()}
+                        disabled={(this.state.WiFiPassword == "" || this.state.WiFiName == "")}
+                    />
+                </View>
+                <View style={{flex: 2}}>
+                    {/* <Text>{this.state.WiFiName}</Text>
+                    <Text>{this.state.WiFiPassword}</Text> */}
                     {error}
                 </View>
                 <NavBar navigation={this.props.navigation} previous='MountingSensor'/>
