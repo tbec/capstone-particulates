c5b31887336b29518ef2a5629fa33c9bd10a7c25
Trevor Chapman <tchapman121@gmail.com>
2019-01-23 07:09:19 -0700

    Work on login

--- AirU_Mobile/Components/Constants.js
+++ AirU_Mobile/Components/Constants.js
@@ -1,2 +1,8 @@
 export const TEST_MODE = true
+
+// Sensor constants
 export const SENSOR_ARRAY = 'Sensors'
+
+// Login constants
+export const LOGIN_NAME = 'LoginName'
+export const LOGIN_TOKEN = 'LoginToken'
--- AirU_Mobile/Views/Setup/Login.js
+++ AirU_Mobile/Views/Setup/Login.js
@@ -3,39 +3,64 @@
  */
 
 import React, {Component} from 'react';
-import {WebView, TouchableHighlight, View, Platform, AsyncStorage} from 'react-native';
+import {TouchableHighlight, View, Text, AsyncStorage,
+         TextInput, Platform, KeyboardAvoidingView, Button, Image, ImageBackground} from 'react-native';
 import styles from '../../StyleSheets/Styles'
 import Icon from 'react-native-vector-icons/Ionicons'
+import { LOGIN_NAME, LOGIN_TOKEN, TEST_MODE } from '../../Components/Constants'
 
 // WIP. Will need to go to correct URL, save token after login, and navigate to next page after done
 export default class Login extends Component<Props> {
+    constructor(props) {
+        super(props);
+        this.login.bind(this)
+        this.state = ({login: '', password: '', error: ''})
+    }
 
     login() {
-        AsyncStorage.setItem('Login', 'Login');
+        // make API call to login passing in login/password
+
+        // check information
+
+        // if success:
+        AsyncStorage.setItem(LOGIN_NAME, this.state.login);
         this.props.navigation.navigate('ReviewFirst');
+
+        // if failed, updated message and display on screen
     }
 
     render() {
         return (
-            <View style={{flex: 1}}>
-                {/* iOS uses localhost, Android uses 10.0.2.2 */}
-                <WebView style={{flex: 10}} source={{uri: Platform.OS === "ios" ? 'http://localhost:5000' 
-                                                        : 'http://10.0.2.2:5000'}}/>
-                <View style={styles.navBar}>
-                    <TouchableHighlight 
-                            style={styles.previousButton}
-                            activeOpacity={30}
-                            underlayColor="yellow"
-                            onPress={() => this.props.navigation.navigate('Sensor')}>
-                                <Icon name={Platform.OS === "ios" ? "ios-close" : "md-close"} size={40}/>
-                    </TouchableHighlight>
-                    <TouchableHighlight 
-                            style={styles.nextButton}
-                            activeOpacity={30}
-                            underlayColor="yellow"
-                            onPress={() => this.login()}>
-                                <Icon name={Platform.OS === "ios" ? "ios-checkmark" : "md-checkmark"} size={40}/>
-                    </TouchableHighlight>
+            <View style={styles.container}>
+                <View style={styles.home}>
+                <Image source={require('../../Resources/red_cloud.jpeg')} 
+                                style={{flex: 2, width: 100, height: 50, alignContent: 'center', justifyContent: 'center'}}/>
+                    <KeyboardAvoidingView style={{flex: 5}}>
+                        <Text>Username</Text>
+                        <TextInput editable={true} keyboardType='default' 
+                                autoCorrect={false} placeholder='Login' secureTextEntry={false} 
+                                style={{borderWidth: 2, borderColor: 'black', 
+                                width: '100%', height: 40, alignContent: 'center', justifyContent: 'center', paddingBottom: 10}}
+                                onChangeText={(value) => {this.setState({login: value})}}
+                                />
+                    </KeyboardAvoidingView>
+                    <KeyboardAvoidingView style={{flex: 5}}>
+                        <Text>Password</Text>
+                        <TextInput editable={true} keyboardType='default' 
+                                autoCorrect={false} placeholder='Password' secureTextEntry={true} 
+                                style={{borderWidth: 2, borderColor: 'black', 
+                                width: '100%', height: 40, alignContent: 'center', justifyContent: 'center', paddingBottom: 10}}
+                                onChangeText={(value) => {this.setState({password: value})}}
+                                />
+                    </KeyboardAvoidingView>
+                    <Button title="Connect"
+                        onPress={() => this.login()}
+                        color='red' 
+                        disabled={(this.state.login == "" || this.state.password == "")}
+                    />
+                    <Text style={{flex: 2, color: "red"}}>
+                        {this.state.error}
+                    </Text>
                 </View>
             </View>
         );
