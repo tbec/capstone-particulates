ee4835c848271cf3505246fd1c9d133cb382f7c7
Trevor Chapman <tchapman121@gmail.com>
2019-02-03 07:56:35 -0700

    Chart pressing

--- AirU_Mobile/Views/Sensor/SensorDisplay.js
+++ AirU_Mobile/Views/Sensor/SensorDisplay.js
@@ -6,7 +6,6 @@ import { SENSOR_ARRAY } from '../../Components/Constants'
 import { BarChart, Grid} from 'react-native-svg-charts'
 import {Text as TextChart, G} from 'react-native-svg'
 
-
 // component should never be called if AsyncStorage.getItem('Sensor') is not already set
 export default class SensorDisplay extends Component<Props> {
     constructor(Props) {
@@ -15,14 +14,15 @@ export default class SensorDisplay extends Component<Props> {
         this.sensorPicker.bind(this);
         this.dataTypeHandler = this.dataTypeHandler.bind(this)
         this.periodHandler = this.periodHandler.bind(this)
-        this.state = {sensorList: [], data: [], selected: '', selectedType: 'Pollution', 
-                        pickingType: false, period: 'hour', connected: true, error: ''}
+        this.graphDataHandler = this.graphDataHandler.bind(this)
+        this.state = {sensorList: [], data: [], selectedSensor: '', selectedType: 'Pollution', 
+                        pickingType: false, period: 'hour', connected: true, error: '', dataPoint: 0}
     }
 
     // gets list of saved sensors
     async getSensors() {
         let sensorsList = await this.getSensorList();
-        this.setState({sensorList: sensorsList, selected: sensorsList[0]})
+        this.setState({sensorList: sensorsList, selectedSensor: sensorsList[0]})
     }
 
     async getSensorList() {
@@ -30,13 +30,14 @@ export default class SensorDisplay extends Component<Props> {
     }
 
     selectSensor(item, index) {
-        this.setState({selected: sensorList[index].id})
+        this.setState({selectedSensor: sensorList[index].id})
     }
 
     componentWillMount() {
         this.getSensors()
     }
 
+    // handlers passed to various functions
     periodHandler(periodRange) {
         this.setState({period: periodRange})
     }
@@ -45,13 +46,17 @@ export default class SensorDisplay extends Component<Props> {
         this.setState({selectedType: dataType})
     }
 
+    graphDataHandler(data) {
+        this.setState({dataPoint: data})
+    }
+
     render() {
         let Pickers = this.sensorPicker();
         
         return (
             <View style={{flex: 3}}>
                 <Text style={{textAlign: 'center', paddingBottom: 10}}>
-                    Connected
+                    Not Connected
                 </Text>
                 <Period period={this.state.period} handler={this.periodHandler}/>
                 <TouchableHighlight testID='dataTypePicker'
@@ -63,9 +68,9 @@ export default class SensorDisplay extends Component<Props> {
                     {this.state.selectedType}
                     </Text>
                 </TouchableHighlight>
-                {/* <DataType handler={this.dataTypeHandler} selected={this.state.selected}/> */}
-                <Graph data={this.state.data}/>
-                <Information data={this.state.data}/>
+                {/* <DataType handler={this.dataTypeHandler} selectedSensor={this.state.selectedSensor}/> */}
+                <Graph data={this.state.data} handler={this.graphDataHandler}/>
+                <Information dataPoint={this.state.dataPoint} selectedType={this.state.selectedType}/>
             </View>
         )
     }
@@ -101,41 +106,62 @@ class Graph extends Component<Props> {
         super(props)
     }
 
+    pollutionColor(pollution) {
+        if (pollution >= 0 && pollution < 25) {
+            return "#00ff00";
+        } else if (pollution >= 25 && pollution < 50) {
+            return "#ffff00";
+        } else if (pollution >= 50 && pollution < 75) {
+            return "#ffa500";
+        } else if (pollution >= 75 && pollution <= 100) {
+            return "#ff0000";
+        } else {
+            return "ff0000";
+        }
+    }
+
     render() {
-        const data = [ 15, 16, 17, 18, 19, 20]
+        const data = [ 50, 10, 40, 95, 4, 24, 85, 91, 35, 53, 53, 24, 50, 20, 80]
 
         const CUT_OFF = 20
-        const Labels = ({ x, y, bandwidth, data }) => (
+        const Labels = ({ x, y, bandwidth, colorData }) => (
             data.map((value, index) => (
-                <G key = { index }>
-                <TextChart
-                    key={ index }
-                    x={ x(index) + (bandwidth / 2) }
-                    y={ value < CUT_OFF ? y(value) - 10 : y(value) + 15 }
-                    fontSize={ 14 }
-                    fill={ value >= CUT_OFF ? 'white' : 'black' }
-                    alignmentBaseline={ 'middle' }
-                    textAnchor={ 'middle' }
-                >
+                <G key = { index }>                    
+                    <TextChart
+                        x={ x(index) + (bandwidth / 2) }
+                        y={ value < CUT_OFF ? y(value) - 10 : y(value) + 15 }
+                        fontSize={ 14 }
+                        fill='black'
+                        // fill={ value >= CUT_OFF ? 'white' : 'black' }
+                        alignmentBaseline={ 'middle' }
+                        textAnchor={ 'middle' }
+                    >
                     {value}
-                </TextChart>
+                    </TextChart>
                 </G>
             ))
         )
 
+        const colorData = data.map((value, index) => ({
+            value,
+            svg: {
+                fill: this.pollutionColor(value),
+                onPress: () => this.props.handler(value)
+            },
+        }))
+
         return (
-            <View style={{ flexDirection: 'row', height: 300, paddingVertical: 5 }}>
+            <View style={{ flexDirection: 'row', height: 300, paddingVertical: 5}}>
                 <BarChart
                     style={{ flex: 1 }}
-                    data={data}
-                    svg={{ fill: 'rgba(134, 65, 244, 1)' }}
+                    data={colorData}
                     contentInset={{ top: 10, bottom: 10 }}
                     spacing={0.2}
                     gridMin={0}
                     yMin={0} 
                     yMax={200}
-                    animate={true}
-                >
+                    yAccessor={({ item }) => item.value}
+                    animate={true}>
                     <Grid direction={Grid.Direction.HORIZONTAL}/>
                     <Labels/>
                 </BarChart>
@@ -187,7 +213,7 @@ class DataType extends Component<Props> {
         <View style={{flex: 1}}>
                 <Picker
                     mode={"dialog"}
-                    selectedValue={this.props.selected} 
+                    selectedSensor={this.props.selectedSensor} 
                     onValueChange={(itemValue, itemIndex) => this.props.handler(itemValue)}>
                     <Picker.Item label="Pollution" value="Pollution"/>
                     <Picker.Item label="Temperature" value="Temperature"/>
@@ -198,7 +224,6 @@ class DataType extends Component<Props> {
     }
 }
 
-
 class Information extends Component<Props> {
     constructor(props) {
         super(props)
@@ -207,7 +232,7 @@ class Information extends Component<Props> {
     render() {
         return(
             <View style={{flex: 1, borderColor: 'black', borderWidth: 1}}>
-                <Text>Information goes here</Text>
+                <Text>{this.props.dataPoint}</Text>
             </View>
         )
     }
