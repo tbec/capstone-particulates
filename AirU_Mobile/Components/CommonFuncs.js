970ba767dd7c3b7f5524b8aab4a9a44c891f269b
Trevor Chapman <tchapman121@gmail.com>
2019-03-11 14:56:18 -0600

    react-native-keychain added

---
+++ AirU_Mobile/Components/CommonFuncs.js
@@ -0,0 +1,45 @@
+import * as Keychain from 'react-native-keychain';
+
+export const accountFuncs = {
+
+     /**Logs into the site, must call before adding device **/
+    async loginKeychain() {
+        let username, password
+
+        await Keychain.getGenericPassword().then((credentials) => {
+            if (pwd == null) {
+                console.error("Password is null")
+            } else {
+                username = credentials.username
+                password = credentials.password
+            }
+        })
+
+        let urlBase = WEB_URL + '/login?'
+        let user = 'username=' + username
+        let passwordParam = '&password=' + password
+
+        let url = urlBase + user + passwordParam
+
+        console.log('URL: ' + url)
+
+        return fetch(url, {method: 'POST', credentials: 'include'})
+            .then((response) => response.json())
+            .then((responseJson) => {
+            return responseJson })
+            .catch((error) => { 
+                console.log(error)
+                return null
+            })
+    },
+
+    /** Saves account details locally */
+    async saveAccount(username, password) {
+        await Keychain.setGenericPassword(username, password)
+    },
+
+    /** Removes account details locally **/
+    async removeAccount() {
+        await Keychain.resetGenericPassword();
+    }
+}
