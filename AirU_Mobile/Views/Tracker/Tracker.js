a4a4b8d184188a9c7596487e909e2887e960af54
connor <connorotte@gmail.com>
2019-03-21 20:17:56 -0600

    got the tracker working

--- AirU_Mobile/Views/Tracker/Tracker.js
+++ AirU_Mobile/Views/Tracker/Tracker.js
@@ -60,8 +60,9 @@ export default class Tracker extends Component {
             stopOnStillActivity: false,
         });
         PushNotification.configure({
-            // (required) Called when a remote or local notification is opened or received
-            onNotification: onNotification, //this._onNotification,
+            onNotification: (notification) => {
+                console.log(notification);
+            }
         });
         if (!this.state.graph) {
             BackgroundGeolocation.getCurrentLocation((position) => {
@@ -167,7 +168,6 @@ export default class Tracker extends Component {
         var d = new Date();
         var n = d.getTime();
         this.setState({ startTracking: false, stopTracking: true, saveData: true, previousTime: n });
-        this.sendNotification();
         BackgroundGeolocation.on('location', (position) => {
             this.animateToRegion(position.latitude, position.longitude);
             let dateTime = this.getCurrentDate();
@@ -245,32 +245,22 @@ export default class Tracker extends Component {
     }
 
     sendNotification() {
+        console.log("send notification called");
         PushNotification.localNotification({
-            ticker: "My Notification Ticker", // (optional)
-            autoCancel: true, // (optional) default: true
-            largeIcon: "ic_launcher", // (optional) default: "ic_launcher"
-            smallIcon: "ic_notification", // (optional) default: "ic_notification" with fallback for "ic_launcher"
-            bigText: "My big text that will be shown when notification is expanded", // (optional) default: "message" prop
-            subText: "This is a subText", // (optional) default: none
-            color: "red", // (optional) default: system default
-            vibrate: true, // (optional) default: true
-            vibration: 300, // vibration length in milliseconds, ignored if vibrate=false, default: 1000
-            tag: 'some_tag', // (optional) add tag to message
-            group: "group", // (optional) add group to message
-            ongoing: false, // (optional) set whether this is an "ongoing" notification
-
-            /* iOS and Android properties */
-            title: "Local Notification", // (optional)
-            message: "My Notification Message", // (required)
-            playSound: false, // (optional) default: true
-            soundName: 'default', // (optional) Sound to play when the notification is shown. Value of 'default' plays the default sound. It can be set to a custom sound such as 'android.resource://com.xyz/raw/my_sound'. It will look for the 'my_sound' audio file in 'res/raw' directory and play it. default: 'default' (default sound is played)
-            number: '10', // (optional) Valid 32 bit integer specified as string. default: none (Cannot be zero)
-            actions: '["Yes", "No"]',  // (Android only) See the doc for notification actions to know more
-        });
-    }
-
-    _onNotification(notification) {
-        console.log(notification);
+            autoCancel: true,
+            largeIcon: "ic_launcher",
+            smallIcon: "ic_notification",
+            bigText: "My big text that will be shown when notification is expanded",
+            subText: "This is a subText",
+            color: "green",
+            vibrate: true,
+            vibration: 300,
+            title: "Notification Title",
+            message: "Notification Message",
+            playSound: true,
+            soundName: 'default',
+            actions: '["Accept", "Reject"]',
+          });
     }
 
     animateToRegion(lat, lng) {
